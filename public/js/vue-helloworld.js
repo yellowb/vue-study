ad1b838649407dc0b11f5de8bf19a9d590e1e3e8
Yellow Huang <yellow.huang@oocl.com>
2017-04-28 09:02:50 +0800

    update

--- public/js/vue-helloworld.js
+++ public/js/vue-helloworld.js
@@ -2,15 +2,48 @@
  * Created by HUANGYE2 on 4/25/2017.
  */
 
+    // 定义名为 todo-item 的新组件
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<p>{{todo}}</p>'
+});
+
+var show = {age: 10};
+
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!',
-        seen: true,
-        listData: [
-            "AAA",
-            "BBB",
-            "CCC"
-        ]
+        el: '#app',
+        data: {
+            message: 'Hello Vue!',
+            show: show,
+            seen: true,
+            listData: [
+                "AAA",
+                "BBB",
+                "CCC"
+            ],
+            todoList: [
+                {
+                    text: 'todo-1'
+                },
+                {
+                    text: 'todo-2'
+                }
+            ]
+        },
+        methods: {
+            click: function () {
+                //this.listData.push('fff');
+                show.b = 'aaa';
+                show.age = 11;
+            }
+        },
+        updated: function() {
+            console.log('Update啦啦啦');
+        }
     }
+);
+
+app.$watch('data', function (newVal, oldVal) {
+    console.log('show updated 啦! ', newVal, ' ', oldVal);
 })
+
