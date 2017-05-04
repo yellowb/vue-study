190defd001bee8eede16960c5a2f48b2bb680dc8
yellowb <122517167@qq.com>
2017-05-04 14:22:06 +0800

    Add v-for sample

---
+++ public/6_ListRendering/v-for.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        people: [
+            {name: 'Tom', age: 20},
+            {name: 'Mary', age: 12},
+            {name: 'Ken', age: 31},
+            {name: 'Billy', age: 200}
+        ]
+    }
+})
