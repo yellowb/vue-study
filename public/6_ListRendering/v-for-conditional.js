bc971e02329c52614743023149dafa0a201b5536
yellowb <122517167@qq.com>
2017-05-04 15:55:54 +0800

    update list render samples

---
+++ public/6_ListRendering/v-for-conditional.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        condition: 0,
+        people: [
+            {name: 'Tom', age: 20},
+            {name: 'Mary', age: 12},
+            {name: 'Ken', age: 31},
+            {name: 'Billy', age: 200}
+        ]
+    },
+    computed: {
+        largerThen: function() {
+            var condition = this.condition;
+            return this.people.filter(function(el) {
+                return el.age > condition;
+            });
+        }
+    }
+})
