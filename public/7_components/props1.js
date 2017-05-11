e335995d4882ac137f1126a7dc81e256c910abb0
yellowb <122517167@qq.com>
2017-05-11 15:57:49 +0800

    update component props1 sample

---
+++ public/7_components/props1.js
@@ -0,0 +1,41 @@
+var com = {
+    template: '<div><div> I am {{ propsName }} ! ' +
+        '<button @click="addWarn">add-warn</button></div>' +
+        '<div> I am {{ localName }} ! ' +
+        '<button @click="addNoWarn">add-no-warn</button></div></div>',
+    props: [
+        'propsName'
+    ],
+    data: function() {
+        return {
+            // use a local variable to be init from props
+            localName: this.propsName
+        };
+    },
+    methods: {
+        'addWarn': function() {
+            // this will cause Vue.js warning
+            this.propsName = this.propsName + 'lo';
+        },
+        'addNoWarn': function() {
+            // this will NOT cause Vue.js warning
+            this.localName = this.localName + 'lo';
+        }
+    },
+    watch: {
+        'propsName': function() {
+            // Need WATCH to sync parent's data.
+            this.localName = this.propsName;
+        }
+    }
+};
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        input: ''
+    },
+    components: {
+        'my-com': com
+    }
+});
